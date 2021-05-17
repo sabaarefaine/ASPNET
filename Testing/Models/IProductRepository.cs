a952e07bc0ab59a9cbe37e644d917e41612ac50e
Saba Arefaine <arefainesaba@gmail.com>
2021-05-16 18:43:53 -0700

    aspnet exercise 1

---
+++ Testing/Models/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Testing.Models
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+    }
+}
